55d72b4a7110a289c9e580d24d1554ce0a25517b
Kin Chan <xkinchanx@gmail.com>
2019-04-24 12:31:16 -0700

    [feat] fake data

---
+++ data/fakePokemonData.js
@@ -0,0 +1,25 @@
+const pokemon = [
+  {
+    id: 12,
+    name: 'butterfree',
+    sprites: {
+      front_default: "http://pokeapi.co/media/sprites/pokemon/12.png"
+    }
+  },
+  {
+    id: 12,
+    name: 'butterfree',
+    sprites: {
+      front_default: "http://pokeapi.co/media/sprites/pokemon/12.png"
+    }
+  },
+  {
+    id: 12,
+    name: 'butterfree',
+    sprites: {
+      front_default: "http://pokeapi.co/media/sprites/pokemon/12.png"
+    }
+  }
+]
+
+export default pokemon;
