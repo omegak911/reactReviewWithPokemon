6ce4bb940d0ab043d8ff638a76889d33dea4483d
Kin Chan <xkinchanx@gmail.com>
2019-04-24 12:32:42 -0700

    [feat] client skelton

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render(){
+    return (
+      <div>
+        Hello from App
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+import fakePokemonData from '../../data/fakePokemonData';
+import getPokemon from '../../lib/getPokemon';
