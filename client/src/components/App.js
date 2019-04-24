d317507699654e2d02bd40a774ef7c03c426b34c
Kin Chan <xkinchanx@gmail.com>
2019-04-24 14:03:57 -0700

    [prog] parent app with state updating functions

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 
+import PokemonList from './PokemonList';
+import Search from './Search';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      pokemonList: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ pokemonList: this.props.pokemonList })
+  }
+
+  getPokemon = (query) => {
+    this.props.getPokemon(query, ({ data }) => {
+      let { id, name, sprites } = data;
+      this.setState({ pokemonList: [...this.state.pokemonList, { id, name, sprites }]})
+    });
+  }
 
   render(){
     return (
       <div>
-        Hello from App
+        <Search getPokemon={this.getPokemon}/>
+        <PokemonList pokemonList={this.state.pokemonList}/>
       </div>
     )
   }
