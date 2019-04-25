08fd430b3777b70bffe3a537e1893d431842039c
Kin Chan <xkinchanx@gmail.com>
2019-04-25 01:38:15 -0700

    [prog] deleted too much, leaving html

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 class Search extends Component {
- 
+  
+  // <div>
+  //   <form action="" >
+  //     <input type="text" />
+  //     <button></button>
+  //   </form>
+  // </div>
+
 }
 
 export default Search;
