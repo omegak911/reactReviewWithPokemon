28435df49d2e686939cd7ae5b49879a682792859
Kin Chan <xkinchanx@gmail.com>
2019-04-25 01:36:52 -0700

    [delete] forgot to delete Search.js solution code

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,34 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 class Search extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      query: ''
-    }
-  }
-
-  handleInput = (e) => {
-    let query = e.target.value;
-    this.setState({ query });
-  }
-
-  search = (e) => {
-    e.preventDefault();
-    this.props.getPokemon(this.state.query.toLowerCase())
-    this.setState({ query: '' });
-  }
-
-  render() {
-    return (
-      <div>
-        <form action="" onSubmit={this.search}>
-          <input type="text" onChange={this.handleInput} value={this.state.query}/>
-          <button type="submit">{this.props.button}</button>
-        </form>
-      </div>
-    )
-  }
+ 
 }
 
 export default Search;
